8466ae3facbf4413fd9797201b179a8e28075785
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2021-12-06 23:04:50 -0500

    Add files via upload

--- Alex.py
+++ Alex.py
@@ -6,51 +6,51 @@ def Alex(skin,mouth,eyes,hair):
 
     #Skin
     for x in range(8):
-        a = Entity(model="cube",color = skin ,position = (x,1,1))
+        a = Entity(model="cube",color = skin ,position = (x,1,0))
 
     for x in range(8):
         if x != 3 and x != 4:
-            a = Entity(model="cube",color = skin ,position = (x,2,1))
+            a = Entity(model="cube",color = skin ,position = (x,2,0))
 
     for x in range(8):
-        a = Entity(model="cube",color = skin ,position = (x,3,1))
+        a = Entity(model="cube",color = skin ,position = (x,3,0))
 
     for x in range(8):
         if x != 1 and x != 2 and x != 5 and x != 6:
-            a = Entity(model="cube",color = skin ,position = (x,4,1))
+            a = Entity(model="cube",color = skin ,position = (x,4,0))
 
     for x in range(3,7):
-        a = Entity(model="cube",color = skin ,position = (x,5,1))
+        a = Entity(model="cube",color = skin ,position = (x,5,0))
 
     for x in range(4,6):
-        a = Entity(model="cube",color = skin ,position = (x,6,1))
+        a = Entity(model="cube",color = skin ,position = (x,6,0))
 
     #Mouth
 
     for x in range(3,5):
-        a = Entity(model="cube",color = mouth ,position = (x,2,1))
+        a = Entity(model="cube",color = mouth ,position = (x,2,0))
 
     #Eyes 
 
-    a = Entity(model="cube",color = color.white ,position = (1,4,1))
-    a = Entity(model="cube",color = eyes ,position = (2,4,1))
+    a = Entity(model="cube",color = color.white ,position = (1,4,0))
+    a = Entity(model="cube",color = eyes ,position = (2,4,0))
 
-    a = Entity(model="cube",color = color.white ,position = (6,4,1))
-    a = Entity(model="cube",color = eyes ,position = (5,4,1))
+    a = Entity(model="cube",color = color.white ,position = (6,4,0))
+    a = Entity(model="cube",color = eyes ,position = (5,4,0))
 
 
     #Hair
     for x in range(8):
         if x != 3 and x != 4 and x != 5 and x != 6:
-            a = Entity(model="cube",color = hair ,position = (x,5,1))
+            a = Entity(model="cube",color = hair ,position = (x,5,0))
 
     for x in range(8):
         if x != 4 and x != 5:
-            a = Entity(model="cube",color = hair ,position = (x,6,1))
+            a = Entity(model="cube",color = hair ,position = (x,6,0))
 
     for x in range(8):
         for y in range(2):
-            a = Entity(model="cube",color = hair ,position = (x,7+y,1))
+            a = Entity(model="cube",color = hair ,position = (x,7+y,0))
 
     EditorCamera()
     app.run()
