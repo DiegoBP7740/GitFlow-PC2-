501e47d37515f6706ec03693db98fa5ab171d8f6
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2021-12-06 22:23:59 -0500

    Add files via upload

---
+++ Blaze.py
@@ -0,0 +1,34 @@
+from ursina import *
+
+app = Ursina()
+
+#Skin 1
+for x in range(8):
+    for y in range(2):
+        b = Entity(model="cube",color = rgb(255,87,51) ,position = (x,y,1))
+
+b = Entity(model="cube",color = rgb(255,87,51) ,position = (7,2,1))
+
+#Skin 2
+
+for x in range(7):
+    b = Entity(model="cube",color = rgb(255,195,0 ) ,position = (x,2,1))
+    
+for x in range(8):
+    if x != 1 and x != 2 and x != 5 and x != 6:
+        b = Entity(model="cube",color = rgb(255,195,0 ) ,position = (x,3,1))
+
+for x in range(8):
+    for y in range(4):
+        b = Entity(model="cube",color = rgb(255,195,0 ) ,position = (x,4+y,1))
+
+#Eyes
+
+b = Entity(model="cube",color = rgb(255,219,102) ,position = (1,3,1))
+b = Entity(model="cube",color = color.black ,position = (2,3,1))
+
+b = Entity(model="cube",color = rgb(255,219,102) ,position = (6,3,1))
+b = Entity(model="cube",color = color.black ,position = (5,3,1))
+
+EditorCamera()
+app.run()
