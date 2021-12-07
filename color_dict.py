b700d6a77b74a4f6faa0c966bef39e10d04effb9
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2021-12-07 18:35:38 -0500

    Add files via upload

---
+++ color_dict.py
@@ -0,0 +1,30 @@
+from ursina import *
+
+colors_dict = {
+
+    1:color.white,
+    2:color.black,
+    3:color.red,
+    4:color.orange,
+    5:color.yellow,
+    6:color.green,
+    7:color.brown,
+    8:color.gold,
+    9:color.dark_gray,
+    10:color.light_gray,
+    11:color.magenta,
+    12:color.pink
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
