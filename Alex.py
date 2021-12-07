82da9db5e3b679e374e156c758b3d82e6cafe7dd
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2021-12-06 21:50:35 -0500

    Add files via upload

--- Alex.py
+++ Alex.py
@@ -1,45 +1,73 @@
 from ursina import *
-
+  
 app = Ursina()
 
 
-#cabello
+#Skin
+for x in range(8):
+    a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,1,1))
+
+for x in range(8):
+    if x != 3 and x != 4:
+        a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,2,1))
+
+for x in range(8):
+    a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,3,1))
+
+for x in range(8):
+    if x != 1 and x != 2 and x != 5 and x != 6:
+        a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,4,1))
+
+for x in range(3,7):
+    a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,5,1))
+
+for x in range(4,6):
+    a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,6,1))
+
+#Mouth
+
+for x in range(3,5):
+    a = Entity(model="cube",color = rgb(251,166,248) ,position = (x,2,1))
+
+#Eyes 
+
+a = Entity(model="cube",color = color.white ,position = (1,4,1))
+a = Entity(model="cube",color = rgb(0,255,18) ,position = (2,4,1))
+
+a = Entity(model="cube",color = color.white ,position = (6,4,1))
+a = Entity(model="cube",color = rgb(0,255,18) ,position = (5,4,1))
+
+
+#Hair
+for x in range(8):
+    if x != 3 and x != 4 and x != 5 and x != 6:
+        a = Entity(model="cube",color = rgb(255,170,0) ,position = (x,5,1))
+
+for x in range(8):
+    if x != 4 and x != 5:
+        a = Entity(model="cube",color = rgb(255,170,0) ,position = (x,6,1))
 
 for x in range(8):
     for y in range(2):
-        c1 = Entity(model="cube",color = rgb(249,174,43),position = (x,6+y,0))
+        a = Entity(model="cube",color = rgb(255,170,0) ,position = (x,7+y,1))
 
-for x in range(3):
-    for y in range(2):
-        c2 = Entity(model="cube",color = rgb(249,174,43),position = (x,4+y,0))
 
-c3 = Entity(model="cube",color = rgb(249,174,43),position = (3,5,0))
 
-for x in range(2):
-    for y in range(1):
-        c4 = Entity(model="cube",color = rgb(249,174,43),position = (6+x,5+y,0))
 
-c5 = Entity(model="cube",color = rgb(249,174,43),position = (7,4,0))
 
-1
-#ojos
-ojo_I = Entity(model="cube",color = rgb(256,256,256),position = (1,3,0))
-pupila_I = Entity(model="cube",color = rgb(25,99,252),position = (2,3,0))
 
-ojo_D = Entity(model="cube",color = rgb(256,256,256),position = (6,3,0))
-pupila_D = Entity(model="cube",color = rgb(25,99,252),position = (5,3,0))
 
-#piel
 
-for x in range(8):
-    for y in range(8):
-        piel = Entity(model="cube",color = rgb(255,226,164),position = (x,y,0))
 
-#boca
-for x in range(2):
-    for y in range(1):
-        piel = Entity(model="cube",color = rgb(251,166,226),position = (x+3,y+1,0))
+
+
+
+
+
+
+
 
 
 EditorCamera()
 app.run()
+
