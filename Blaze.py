1f9431ac23a07897a5ed7f4b5ccf9feb89d416a1
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2021-12-08 16:42:10 -0500

    Add files via upload

--- Blaze.py
+++ Blaze.py
@@ -1,34 +1,72 @@
 from ursina import *
 
-app = Ursina()
 
-#Skin 1
-for x in range(8):
-    for y in range(2):
-        b = Entity(model="cube",color = rgb(255,87,51) ,position = (x,y,1))
-
-b = Entity(model="cube",color = rgb(255,87,51) ,position = (7,2,1))
-
-#Skin 2
-
-for x in range(7):
-    b = Entity(model="cube",color = rgb(255,195,0 ) ,position = (x,2,1))
+def Blaze_default():
     
-for x in range(8):
-    if x != 1 and x != 2 and x != 5 and x != 6:
-        b = Entity(model="cube",color = rgb(255,195,0 ) ,position = (x,3,1))
+    app = Ursina()
 
-for x in range(8):
-    for y in range(4):
-        b = Entity(model="cube",color = rgb(255,195,0 ) ,position = (x,4+y,1))
+    #Skin 1
+    for x in range(8):
+        for y in range(2):
+            b = Entity(model="cube",color = rgb(255,87,51) ,position = (x,y,1))
 
-#Eyes
+    b = Entity(model="cube",color = rgb(255,87,51) ,position = (7,2,1))
 
-b = Entity(model="cube",color = rgb(255,219,102) ,position = (1,3,1))
-b = Entity(model="cube",color = color.black ,position = (2,3,1))
+    #Skin 2
 
-b = Entity(model="cube",color = rgb(255,219,102) ,position = (6,3,1))
-b = Entity(model="cube",color = color.black ,position = (5,3,1))
+    for x in range(7):
+        b = Entity(model="cube",color = rgb(255,195,0 ) ,position = (x,2,1))
+        
+    for x in range(8):
+        if x != 1 and x != 2 and x != 5 and x != 6:
+            b = Entity(model="cube",color = rgb(255,195,0 ) ,position = (x,3,1))
 
-EditorCamera()
-app.run()
+    for x in range(8):
+        for y in range(4):
+            b = Entity(model="cube",color = rgb(255,195,0 ) ,position = (x,4+y,1))
+
+    #Eyes
+
+    b = Entity(model="cube",color = rgb(255,219,102) ,position = (1,3,1))
+    b = Entity(model="cube",color = color.black ,position = (2,3,1))
+
+    b = Entity(model="cube",color = rgb(255,219,102) ,position = (6,3,1))
+    b = Entity(model="cube",color = color.black ,position = (5,3,1))
+
+    EditorCamera()
+    app.run()
+
+def Blaze(primary_color,secondary_color):
+    app = Ursina()
+
+    #Skin 1
+    for x in range(8):
+        for y in range(2):
+            b = Entity(model="cube",color = secondary_color ,position = (x,y,1))
+
+    b = Entity(model="cube",color = secondary_color ,position = (7,2,1))
+
+    #Skin 2
+
+    for x in range(7):
+        b = Entity(model="cube",color = primary_color ,position = (x,2,1))
+        
+    for x in range(8):
+        if x != 1 and x != 2 and x != 5 and x != 6:
+            b = Entity(model="cube",color = primary_color ,position = (x,3,1))
+
+    for x in range(8):
+        for y in range(4):
+            b = Entity(model="cube",color = primary_color ,position = (x,4+y,1))
+
+    #Eyes
+
+    b = Entity(model="cube",color = rgb(255,219,102) ,position = (1,3,1))
+    b = Entity(model="cube",color = color.black ,position = (2,3,1))
+
+    b = Entity(model="cube",color = rgb(255,219,102) ,position = (6,3,1))
+    b = Entity(model="cube",color = color.black ,position = (5,3,1))
+
+    EditorCamera()
+    app.run()
+
