32717f80f4d4b49ab28b5f0f688f69ed580b0e64
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2021-12-08 15:52:28 -0500

    Add files via upload

--- Alex.py
+++ Alex.py
@@ -56,3 +56,58 @@ def Alex(skin,mouth,eyes,hair):
     app.run()
 
 
+def Alex_defalult():
+    app = Ursina()
+
+    #Skin
+    for x in range(8):
+        a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,1,0))
+
+    for x in range(8):
+        if x != 3 and x != 4:
+            a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,2,0))
+
+    for x in range(8):
+        a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,3,0))
+
+    for x in range(8):
+        if x != 1 and x != 2 and x != 5 and x != 6:
+            a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,4,0))
+
+    for x in range(3,7):
+        a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,5,0))
+
+    for x in range(4,6):
+        a = Entity(model="cube",color = rgb(251,211,166) ,position = (x,6,0))
+
+    #Mouth
+
+    for x in range(3,5):
+        a = Entity(model="cube",color = rgb(251,166,248) ,position = (x,2,0))
+
+    #Eyes 
+
+    a = Entity(model="cube",color = color.white ,position = (1,4,0))
+    a = Entity(model="cube",color = rgb(0,255,18) ,position = (2,4,0))
+
+    a = Entity(model="cube",color = color.white ,position = (6,4,0))
+    a = Entity(model="cube",color = rgb(0,255,18) ,position = (5,4,0))
+
+
+    #Hair
+    for x in range(8):
+        if x != 3 and x != 4 and x != 5 and x != 6:
+            a = Entity(model="cube",color = rgb(255,170,0) ,position = (x,5,0))
+
+    for x in range(8):
+        if x != 4 and x != 5:
+            a = Entity(model="cube",color = rgb(255,170,0) ,position = (x,6,0))
+
+    for x in range(8):
+        for y in range(2):
+            a = Entity(model="cube",color = rgb(255,170,0) ,position = (x,7+y,0))
+
+    EditorCamera()
+    app.run()
+
+
