67bea53c72d00ccf0e7e7359a9febb000b7423a6
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2021-12-09 12:44:16 -0500

    Add files via upload

--- Creeper.py
+++ Creeper.py
@@ -1,63 +1,95 @@
 from ursina import *
 def Creeper_default():
     app = Ursina()
+    #NARIZ
+    b = Entity(model="cube",color = color.black ,position = (3,3,0))
+    b = Entity(model="cube",color = color.black ,position = (4,3,0))
+    #boca
+    b = Entity(model="cube",color = color.black ,position = (4,2,0))
+    b = Entity(model="cube",color = color.black ,position = (4,1,0))
+    b = Entity(model="cube",color = color.black ,position = (5,2,0))
+    b = Entity(model="cube",color = color.black ,position = (3,2,0))
+    b = Entity(model="cube",color = color.black ,position = (2,2,0))
+    b = Entity(model="cube",color = color.black ,position = (3,1,0))
+    b = Entity(model="cube",color = color.black ,position = (2,1,0))
+    b = Entity(model="cube",color = color.black ,position = (5,1,0))
+    b = Entity(model="cube",color = color.black ,position = (5,0,0))
+    b = Entity(model="cube",color = color.black ,position = (2,0,0))
+    #OJOS
+    b = Entity(model="cube",color = color.black ,position = (1,5,0))
+    b = Entity(model="cube",color = color.black ,position = (2,5,0))
+    b = Entity(model="cube",color = color.black ,position = (2,4,0))
+    b = Entity(model="cube",color = color.black ,position = (1,4,0))
+    b = Entity(model="cube",color = color.black ,position = (5,5,0))
+    b = Entity(model="cube",color = color.black ,position = (6,5,0))
+    b = Entity(model="cube",color = color.black ,position = (6,4,0))
+    b = Entity(model="cube",color = color.black ,position = (5,4,0))
+    #rostro de fondo
+    
     for y in range(0,8):
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(0, y, 0))
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(7, y, 0))
-    for y in range(0,3):
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(1, y, 0))
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(2, y, 0))
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(5, y, 0))
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(6, y, 0))
-    for y in range(4,8):
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(1, y, 0))
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(2, y, 0))
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(5, y, 0))
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(6, y, 0))
-    for y in range(3,8):
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(3, y, 0))
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(4, y, 0))
-    for y in range(0,2):
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(3, y, 0))
-        b = Entity(model="cube", color=rgb(84, 197, 113), position=(4, y, 0))
-    #ojos
-    b = Entity(model="cube", color=color.black, position=(1, 3, 0))
-    b = Entity(model="cube", color=color.black, position=(2, 3, 0))
-    b = Entity(model="cube", color=color.black, position=(5, 3, 0))
-    b = Entity(model="cube", color=color.black, position=(6, 3, 0))
-
-    b = Entity(model="cube", color=rgb(0,143,57), position=(3, 2, 0))
-    b = Entity(model="cube", color=rgb(0,143,57), position=(4, 2, 0))
+        b = Entity(model="cube",color =rgb(0, 187, 45)  ,position = (0,y,0))
+        b = Entity(model="cube", color=rgb(0, 187, 45), position=(7, y, 0))
+    for y in range(0, 4):
+        b = Entity(model="cube", color=rgb(0, 187, 45), position=(1, y, 0))
+        b = Entity(model="cube", color=rgb(0, 187, 45), position=(6, y, 0))
+    for x in range (3,5):
+        b = Entity(model="cube",color = rgb(0,187,45) ,position = (x,0,0))
+    for x in range (1,7):
+        b = Entity(model="cube",color = rgb(0,187,45) ,position = (x,7,0))
+        b = Entity(model="cube", color=rgb(0, 187, 45), position=(x, 6, 0))
+    
+    b = Entity(model="cube",color = rgb(0, 187, 45) ,position = (3,5,0))
+    b = Entity(model="cube",color = rgb(0, 187, 45) ,position = (4,5,0))
+    b = Entity(model="cube",color = rgb(0, 187, 45) ,position = (3,4,0))
+    b = Entity(model="cube",color = rgb(0, 187, 45) ,position = (4,4,0))
+    b = Entity(model="cube",color = rgb(0, 187, 45) ,position = (2,3,0))
+    b = Entity(model="cube",color = rgb(0, 187, 45) ,position = (5,3,0))
     EditorCamera()
     app.run()
 def Creeper(primary_color, secondary_color):
     app = Ursina()
+    # NARIZ
+    b = Entity(model="cube", color=secondary_color, position=(3, 3, 0))
+    b = Entity(model="cube", color=secondary_color, position=(4, 3, 0))
+    # boca
+    b = Entity(model="cube", color=secondary_color, position=(4, 2, 0))
+    b = Entity(model="cube", color=secondary_color, position=(4, 1, 0))
+    b = Entity(model="cube", color=secondary_color, position=(5, 2, 0))
+    b = Entity(model="cube", color=secondary_color, position=(3, 2, 0))
+    b = Entity(model="cube", color=secondary_color, position=(2, 2, 0))
+    b = Entity(model="cube", color=secondary_color, position=(3, 1, 0))
+    b = Entity(model="cube", color=secondary_color, position=(2, 1, 0))
+    b = Entity(model="cube", color=secondary_color, position=(5, 1, 0))
+    b = Entity(model="cube", color=secondary_color, position=(5, 0, 0))
+    b = Entity(model="cube", color=secondary_color, position=(2, 0, 0))
+    # OJOS
+    b = Entity(model="cube", color=secondary_color, position=(1, 5, 0))
+    b = Entity(model="cube", color=secondary_color, position=(2, 5, 0))
+    b = Entity(model="cube", color=secondary_color, position=(2, 4, 0))
+    b = Entity(model="cube", color=secondary_color, position=(1, 4, 0))
+    b = Entity(model="cube", color=secondary_color, position=(5, 5, 0))
+    b = Entity(model="cube", color=secondary_color, position=(6, 5, 0))
+    b = Entity(model="cube", color=secondary_color, position=(6, 4, 0))
+    b = Entity(model="cube", color=secondary_color, position=(5, 4, 0))
+    # rostro de fondo
+
     for y in range(0, 8):
         b = Entity(model="cube", color=primary_color, position=(0, y, 0))
         b = Entity(model="cube", color=primary_color, position=(7, y, 0))
-    for y in range(0, 3):
+    for y in range(0, 4):
         b = Entity(model="cube", color=primary_color, position=(1, y, 0))
-        b = Entity(model="cube", color=primary_color, position=(2, y, 0))
-        b = Entity(model="cube", color=primary_color, position=(5, y, 0))
         b = Entity(model="cube", color=primary_color, position=(6, y, 0))
-    for y in range(4, 8):
-        b = Entity(model="cube", color=primary_color, position=(1, y, 0))
-        b = Entity(model="cube", color=primary_color, position=(2, y, 0))
-        b = Entity(model="cube", color=primary_color, position=(5, y, 0))
-        b = Entity(model="cube", color=primary_color, position=(6, y, 0))
-    for y in range(3, 8):
-        b = Entity(model="cube", color=primary_color, position=(3, y, 0))
-        b = Entity(model="cube", color=primary_color, position=(4, y, 0))
-    for y in range(0, 2):
-        b = Entity(model="cube", color=primary_color, position=(3, y, 0))
-        b = Entity(model="cube", color=primary_color, position=(4, y, 0))
-    # ojos
-    b = Entity(model="cube", color=color.black, position=(1, 3, 0))
-    b = Entity(model="cube", color=color.black, position=(2, 3, 0))
-    b = Entity(model="cube", color=color.black, position=(5, 3, 0))
-    b = Entity(model="cube", color=color.black, position=(6, 3, 0))
+    for x in range(3, 5):
+        b = Entity(model="cube", color=primary_color, position=(x, 0, 0))
+    for x in range(1, 7):
+        b = Entity(model="cube", color=primary_color, position=(x, 7, 0))
+        b = Entity(model="cube", color=primary_color, position=(x, 6, 0))
 
-    b = Entity(model="cube", color=secondary_color, position=(3, 2, 0))
-    b = Entity(model="cube", color=secondary_color, position=(4, 2, 0))
+    b = Entity(model="cube", color=primary_color, position=(3, 5, 0))
+    b = Entity(model="cube", color=primary_color, position=(4, 5, 0))
+    b = Entity(model="cube", color=primary_color, position=(3, 4, 0))
+    b = Entity(model="cube", color=primary_color, position=(4, 4, 0))
+    b = Entity(model="cube", color=primary_color, position=(2, 3, 0))
+    b = Entity(model="cube", color=primary_color, position=(5, 3, 0))
     EditorCamera()
     app.run()
