babc29f80b51f8aad469f9f2459904a884f5a142
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2021-12-09 12:00:13 -0500

    Add files via upload

--- Skin_Minecraft_Creator.py
+++ Skin_Minecraft_Creator.py
@@ -1,32 +1,89 @@
 from ursina import *
 from menu2 import *
+from menu2 import *
+from menu3 import *
 
 print("Skin Minecraft Creator")
 
 name = input("Username: ")
-
+print("")
 print(f"Welcome {name} !!")
 print("")
 print("Choose your character ")
 print("")
-print("[1] Steve")
-print("[2] Alex")
-print("[3] Enderman")
-print("[4] Creeper")
+print(f"[1] Steve      [7] Pig")
+print(f"[2] Alex       [8] Magma")
+print(f"[3] Creeper    [9] Blaze")
+print(f"[4] Zombie     [10] Sheep")
+print(f"[5] Skeleton   [11] Spider")
+print(f"[6] Cow")        
+
 print("")
 option = int(input("Send your option: "))
+print("")
 
 if option == 1:
     print(f"Opcion selected [Steve] ")
-elif option ==2:
+    menu2("Steve")
+
+elif option == 2:
     print(f"Opcion selected [Alex] ")
     print("")
-    menu2(Alex)
-    
+    print(menu2("Alex"))
 
 elif option == 3:
-    print(f"Opcion selected [Enderman] ")
-elif option == 4:
     print(f"Opcion selected [Creeper] ")
+    print("")
+    print(menu3("Creeper"))
+
+elif option == 4:
+    print(f"Opcion selected [Zombie] ")
+    print("")
+    print(menu3("Zombie"))
+
+elif option == 5:
+    print(f"Opcion selected [Skeleton] ")
+    print("")
+    print(menu3("Skeleton"))
+
+elif option == 6:
+    print(f"Opcion selected [Cow] ")
+    print("")
+    print(menu3("Cow"))
+
+elif option == 7:
+    print(f"Opcion selected [Pig] ")
+    print("")
+    print(menu3("Pig"))
+
+elif option == 8:
+    print(f"Opcion selected [Magma] ")
+    print("")
+    print(menu3("Magma"))
+
+elif option == 9:
+    print(f"Opcion selected [Blaze] ")
+    print("")
+    print(menu3("Blaze"))
+
+elif option == 10:
+    print(f"Opcion selected [Sheep] ")
+    print("")
+    print(menu3("Sheep"))
+
+elif option == 11:
+    print(f"Opcion selected [Spider] ")
+    print("")
+    print(menu3("Spider"))
+
+
+else:
+    print("ERROR 404 :(")
+
+    
+    
+
+
+
 
 
