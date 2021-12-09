f0f9d5369d1b57240e6fbd95ac17280ce39d60b2
Gian Marco <91270081+2030Gian@users.noreply.github.com>
2021-12-08 22:48:49 -0500

    Calamar
    Hola, soy Gian: 
    Ya esta listo el calamar

---
+++ Calamar.py
@@ -0,0 +1,99 @@
+from ursina import *
+
+app = Ursina()
+
+camera.orthographic = True
+
+#fila 0
+
+Entity(model='cube', color=color.blue, position=(1,0))
+Entity(model='cube', color=color.blue, position=(2,0))
+Entity(model='cube', color=color.blue, position=(3,0))
+Entity(model='cube', color=color.blue, position=(4,0))
+Entity(model='cube', color=color.blue, position=(5,0))
+Entity(model='cube', color=color.blue, position=(6,0))
+Entity(model='cube', color=color.blue, position=(7,0))
+Entity(model='cube', color=color.blue, position=(8,0))
+
+#columna 1
+Entity(model='cube', color=color.blue, position=(1,1))
+Entity(model='cube', color=color.blue, position=(1,2))
+Entity(model='cube', color=color.blue,position=(1,3))
+Entity(model='cube', color=color.blue, position=(1,4))
+Entity(model='cube', color=color.blue, position=(1,5))
+Entity(model='cube', color=color.blue, position=(1,6))
+Entity(model='cube', color=color.blue, position=(1,7))
+
+#columna 2
+Entity(model='cube', color=color.blue, position=(2,1))
+Entity(model='cube', color=color.azure, position=(2,2))
+Entity(model='cube', color=color.azure, position=(2,3))
+Entity(model='cube', color=color.white, position=(2,4))
+Entity(model='cube', color=color.black, position=(2,5))
+Entity(model='cube', color=color.blue, position=(2,6))
+Entity(model='cube', color=color.blue, position=(2,7))
+
+#columna 3
+
+Entity(model='cube', color=color.azure, position=(3, 1))
+Entity(model='cube', color=color.azure, position=(3, 2))
+Entity(model='cube', color=color.azure, position=(3, 3))
+Entity(model='cube', color=color.white, position=(3, 4))
+Entity(model='cube', color=color.white, position=(3, 5))
+Entity(model='cube', color=color.azure, position=(3, 6))
+Entity(model='cube', color=color.blue, position=(3, 7))
+
+#columna 4
+
+Entity(model='cube', color=color.azure, position=(4, 1))
+Entity(model='cube', color=color.azure, position=(4, 2))
+Entity(model='cube', color=color.azure, position=(4, 3))
+Entity(model='cube', color=color.azure, position=(4, 4))
+Entity(model='cube', color=color.azure, position=(4, 5))
+Entity(model='cube', color=color.azure, position=(4, 6))
+Entity(model='cube', color=color.blue, position=(4, 7))
+
+#columna 5
+
+Entity(model='cube', color=color.azure, position=(5, 1))
+Entity(model='cube', color=color.azure, position=(5, 2))
+Entity(model='cube', color=color.azure, position=(5, 3))
+Entity(model='cube', color=color.azure, position=(5, 4))
+Entity(model='cube', color=color.azure, position=(5, 5))
+Entity(model='cube', color=color.azure, position=(5, 6))
+Entity(model='cube', color=color.blue, position=(5, 7))
+
+#columna 6
+
+Entity(model='cube', color=color.azure, position=(6, 1))
+Entity(model='cube', color=color.azure, position=(6, 2))
+Entity(model='cube', color=color.azure, position=(6, 3))
+Entity(model='cube', color=color.white, position=(6, 4))
+Entity(model='cube', color=color.white, position=(6, 5))
+Entity(model='cube', color=color.azure, position=(6, 6))
+Entity(model='cube', color=color.blue, position=(6, 7))
+
+#columna 7
+
+Entity(model='cube', color=color.blue, position=(7, 1))
+Entity(model='cube', color=color.azure, position=(7, 2))
+Entity(model='cube', color=color.azure, position=(7, 3))
+Entity(model='cube', color=color.white, position=(7, 4))
+Entity(model='cube', color=color.black, position=(7, 5))
+Entity(model='cube', color=color.blue, position=(7, 6))
+Entity(model='cube', color=color.blue, position=(7, 7))
+
+
+#columna 8
+
+Entity(model='cube', color=color.blue, position=(8, 1))
+Entity(model='cube', color=color.blue, position=(8, 2))
+Entity(model='cube', color=color.blue, position=(8, 3))
+Entity(model='cube', color=color.blue, position=(8, 4))
+Entity(model='cube', color=color.blue, position=(8, 5))
+Entity(model='cube', color=color.blue, position=(8, 6))
+Entity(model='cube', color=color.blue, position=(8, 7))
+
+EditorCamera()
+
+app.run()
